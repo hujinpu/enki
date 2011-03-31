b53d4198082469c6c3baf4c1be318a3fd2408993
Jinpu Hu <hujinpu@gmail.com>
2011-04-01 00:39:08 +0800

    use local jquery

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -3,13 +3,13 @@ Function.prototype.only_every = function (millisecond_delay) {
   if (!window.only_every_func)
   {
     var function_object = this;
-    window.only_every_func = setTimeout(function() { function_object(); window.only_every_func = null}, millisecond_delay);
+    window.only_every_func = setTimeout(function() { function_object(); window.only_every_func = null;}, millisecond_delay);
    }
 };
 
 // http://www.ivanuzunov.net/top-10-javascript-stringprototype-extensions/
 String.prototype.endsWith = function(t, i) {
-  if (i==false) {
+  if (i === false) {
     return (t == this.substring(this.length - t.length));
   } else {
     return (t.toLowerCase() == this.substring(this.length - t.length).toLowerCase());
@@ -24,4 +24,4 @@ jQuery.ajaxSetup({
 // jQuery extensions
 jQuery.prototype.any = function(callback) {
   return (this.filter(callback).length > 0);
-}
+};
